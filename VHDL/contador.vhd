0b4b3a5728d83ccac0856ce8f1f2d588b343087e
Gustavo <45674528+gustavo-sen@users.noreply.github.com>
2023-05-15 10:24:22 -0300

    Update contador.vhd

--- VHDL/contador.vhd
+++ VHDL/contador.vhd
@@ -3,7 +3,6 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-
 entity contador is
 	port(
 		clk, rst: in std_logic;
@@ -12,24 +11,20 @@ entity contador is
 end contador;
 
 architecture ctt of contador is
+
 	signal q_temp : unsigned (7 downto 0);
 	
 	begin
-	
-	process(clk,rst)
-		begin
+		process(clk,rst)
+			begin
 		
-		if rst = '1' then
-			q_temp <= (others => '0'); -- aplica o valor 0 a todas as posições do vetor
-			
-		elsif rising_edge(clk) then 
-			
-			q_temp <= (q_temp + '1'); -- biblioteca STD permite concatenar '1' (bit) e 1 (integer)
-			
-		end if;
+			if rst = '1' then
+				q_temp <= (others => '0'); -- aplica o valor 0 a todas as posições do vetor
+			elsif rising_edge(clk) then 
+				q_temp <= (q_temp + '1'); -- Concatena '1'(bit) com 1 (integer) [1 + 1 = 11]
+			end if;
 		
-	end process;
-	
-		q <= std_logic_vector(q_temp); -- converte 'unsigned' para 'std_logic_vector'
-	
+		end process;
+	q <= std_logic_vector(q_temp); -- converte 'unsigned' --> 'std_logic_vector'
+
 end ctt;
